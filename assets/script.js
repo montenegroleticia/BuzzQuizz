c1179b2363ccb9dd8103510e67cae65190db8325
joao <joaosb004@gmail.com>
2023-01-27 14:35:47 -0300

    exibição do quizz pronta

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 // Obter todos os quizzes
+
 function carregarQuizz(resposta) {
-    console.log(resposta.data);
     const quizz = document.querySelector('ul');
     quizz.innerHTML = '';
     for (let index = 0; index < resposta.data.length; index++) {
@@ -36,30 +36,140 @@ function nãoAbriuQuizz(erro) {
 }
 
 function embaralharRespotas(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+    let m = array.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+
+        // And swap it with the current element.
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
     }
+
+    return array;
 }
 
 // exibir quiz selecionado
+
 function exibirQuizz(selectedQuizz) {
 
     selectedQuizz = selectedQuizz.data;
 
-    const main = document.querySelector("main");
+    document.querySelector("main").classList.add("larguraTotal");
+    document.querySelector(".tela01").classList.add('hide');
+    document.querySelector(".tela02").classList.remove('hide');
+    document.querySelector(".tela03").classList.add('hide');
 
-    main.innerHTML = `<figure class="tituloQuiz">
+
+    const tela02 = document.querySelector(".tela02");
+    tela02.innerHTML = `<figure class="tituloQuiz">
                         <h1>${selectedQuizz.title}</h1>
                       </figure>`;
 
     document.querySelector(".tituloQuiz").style.background =
         `linear-gradient(0deg, rgba(0, 0, 0, 0.60), rgba(0, 0, 0, 0.60)), url('${selectedQuizz.image})`;
 
-    embaralharRespotas(selectedQuizz.questions);
+    for (let index = 0; index < selectedQuizz.questions.length; index++) {
+        selectedQuizz.questions[index].answers = embaralharRespotas(selectedQuizz.questions[index].answers);
+    }
 
-    selectedQuizz.questions.forEach(answers => {
-        if()
+    selectedQuizz.questions.forEach(perguntas => {
+
+        let res = [];
+
+        let titulo = `<section class="pergunta">
+                        <div class="color" style="background-color: ${perguntas.color};">
+                            <h1>${perguntas.title}</h1>
+                        </div>`;
+
+        function respostaCorreta(element) {
+            if(element.isCorrectAnswer)
+                    res.push('respostaCerta');
+                else
+                    res.push('respostaErrada');
+        }
+
+        if (perguntas.answers.length === 2) {
+
+            perguntas.answers.forEach(respostaCorreta);
+
+            tela02.innerHTML += titulo +
+            `   <div class="flex">
+                    <div class="miniContainer ${res[0]}" onclick="tratarEscolha(this)">
+                        <img src="${perguntas.answers[0].image}" alt="imagem">
+                        <p>${perguntas.answers[0].text}</p>
+                    </div>
+                    <div class="miniContainer margin ${res[1]}" onclick="tratarEscolha(this)">
+                        <img src="${perguntas.answers[1].image}" alt="imagem">
+                        <p>${perguntas.answers[1].text}</p>
+                    </div>
+                </div>
+            </section>`
+
+            res = [];
+        }
+        else if (perguntas.answers.length === 3) {
+            
+            perguntas.answers.forEach(respostaCorreta);
+
+            tela02.innerHTML += titulo;
+            `   <div class="flex">
+                    <div>
+                        <div class="miniContainer ${res[0]}" onclick="tratarEscolha(this)">
+                            <img src="${perguntas.answers[0].image}" alt="imagem">
+                            <p>${perguntas.answers[0].text}</p>
+                        </div>
+                        <div class="miniContainer ${res[1]}" onclick="tratarEscolha(this)">
+                            <img src="${perguntas.answers[1].image}" alt="imagem">
+                            <p>${perguntas.answers[1].text}</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="miniContainer margin ${res[2]}" onclick="tratarEscolha(this)">
+                            <img src="${perguntas.answers[2].image}" alt="imagem">
+                            <p>${perguntas.answers[2].text}</p>
+                        </div>
+                    </div>
+                </div>
+            </section>`
+
+            res = [];
+        }
+        else if (perguntas.answers.length === 4) {
+
+            perguntas.answers.forEach(respostaCorreta);
+
+            tela02.innerHTML += titulo +
+            `   <div class="flex">
+                    <div>
+                        <div class="miniContainer ${res[0]}" onclick="tratarEscolha(this)">
+                            <img src="${perguntas.answers[0].image}" alt="imagem">
+                            <p>${perguntas.answers[0].text}</p>
+                        </div>
+                        <div class="miniContainer ${res[1]}" onclick="tratarEscolha(this)">
+                            <img src="${perguntas.answers[1].image}" alt="imagem">
+                            <p>${perguntas.answers[1].text}</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="miniContainer margin ${res[2]}" onclick="tratarEscolha(this)">
+                            <img src="${perguntas.answers[2].image}" alt="imagem">
+                            <p>${perguntas.answers[2].text}</p>
+                        </div>
+                        <div class="miniContainer margin ${res[3]}" onclick="tratarEscolha(this)">
+                            <img src="${perguntas.answers[3].image}" alt="imagem">
+                            <p>${perguntas.answers[3].text}</p>
+                        </div>
+                    </div>
+                </div>
+            </section>`
+            
+            res = [];
+        }
     });
 
 }
@@ -67,7 +177,6 @@ function exibirQuizz(selectedQuizz) {
 // Selecionar um quizz específico ao clicar
 function quizzSelecionado(selecionado) {
     const quiz = Number(selecionado.id);
-    console.log(quiz);
     const promese = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${quiz}`);
     promese.then(exibirQuizz);
     promese.catch(nãoAbriuQuizz);
